607dac8578d0612c0755c80e610fc3b89fde5209
Movsar Bekaev <bekav.movsar@gmail.com>
2017-09-28 12:11:00 +0300

    errors

--- CopyNoMatterWhat/Program.cs
+++ CopyNoMatterWhat/Program.cs
@@ -11,44 +11,48 @@ namespace CopyNoMatterWhat
 {
     public class Program
     {
+        private static DriveInfo di;
+        private static int DELAY = 500;
+
         private static void DirectoryCopy(string sourceDirName, string destDirName, bool copySubDirs)
         {
             // Get the subdirectories for the specified directory.
             DirectoryInfo dir = new DirectoryInfo(sourceDirName);
 
-            //TryAgain:
-            //if (!(new DriveInfo("f:").IsReady)) {
-            //    //throw new DirectoryNotFoundException("Source directory does not exist or could not be found: " + sourceDirName);
-            //    Thread.Sleep(200);
-            //    goto TryAgain;
-            //}
-
-            DirectoryInfo[] dirs = dir.GetDirectories();
-            // If the destination directory doesn't exist, create it.
-            if (!Directory.Exists(destDirName)) {
-                Directory.CreateDirectory(destDirName);
-            }
-
-            // Get the files in the directory and copy them to the new location.
-            FileInfo[] files = dir.GetFiles();
-            foreach (FileInfo file in files) {
-                string temppath = Path.Combine(destDirName, file.Name);
-                copy(file.FullName, temppath);
-                Console.WriteLine(file.Name + "...OK");
-            }
-
-            // If copying subdirectories, copy them and their contents to new location.
-            if (copySubDirs) {
-                foreach (DirectoryInfo subdir in dirs) {
-                    string temppath = Path.Combine(destDirName, subdir.Name);
-                    DirectoryCopy(subdir.FullName, temppath, copySubDirs);
+            TryAgainDriveIsNotReady:
+            try {
+                DirectoryInfo[] dirs = dir.GetDirectories();
+                // If the destination directory doesn't exist, create it.
+                if (!Directory.Exists(destDirName)) {
+                    Directory.CreateDirectory(destDirName);
                 }
+
+                // Get the files in the directory and copy them to the new location.
+                FileInfo[] files = dir.GetFiles();
+                foreach (FileInfo file in files) {
+                    string temppath = Path.Combine(destDirName, file.Name);
+                    if (!(new FileInfo(temppath).Exists)) copy(file.FullName, temppath, file.Length);
+                }
+
+                // If copying subdirectories, copy them and their contents to new location.
+                if (copySubDirs) {
+                    foreach (DirectoryInfo subdir in dirs) {
+                        string temppath = Path.Combine(destDirName, subdir.Name);
+                        DirectoryCopy(subdir.FullName, temppath, copySubDirs);
+                    }
+                }
+                Debug.WriteLine("Done");
+            } catch (Exception ex) {
+                Thread.Sleep(DELAY);
+                Debug.WriteLine("Get Directories Fail");
+                goto TryAgainDriveIsNotReady;
             }
         }
 
 
         static void Main(string[] args)
         {
+            di = new DriveInfo("f:");
             /*  string driveName = Console.ReadLine();
               DriveInfo di = new DriveInfo(driveName);
               (new DirectoryInfo("out")).Create();
@@ -63,46 +67,77 @@ namespace CopyNoMatterWhat
 
       */
 
-            DirectoryCopy(@"D:\_Images\Мотивация\", "out", true);
+            DirectoryCopy(@"F:\", "out", true);
         }
 
-        static int currentByte = 0;
-        private static void copy(string path, string name)
+        private static void copy(string path, string name, long length)
         {
+            byte[] data = null;
+            int bufferSize = 1024;
+            Console.WriteLine("Читаю " + path);
+
             FileStream fin;
-            int index = 0;
+            long index = 0;
 
-            const int arrayLength = 1000;
-            byte[] dataArray = new byte[arrayLength];
-            byte[] verifyArray = new byte[arrayLength];
+            TryAgain1: try {
+                if (di.IsReady)
+                    fin = new FileStream(path, FileMode.Open);
+                else {
+                    Debug.WriteLine("Drive is not ready");
+                    Thread.Sleep(DELAY);
+                    goto TryAgain1;
+                }
+            } catch (IOException ex) {
+                Debug.WriteLine("TA 1: " + ex.Message);
+                Thread.Sleep(DELAY);
+                goto TryAgain1;
+            }
 
-            //        TryAgain1: try {
-            fin = new FileStream(path, FileMode.Open);
-            byte[] data;
+
             using (BinaryReader binReader = new BinaryReader(fin, new ASCIIEncoding())) {
-                binReader.BaseStream.Position = index;
+                byte[] chunk;
+                TryAgain2: try {
+                    binReader.BaseStream.Position = index;
+                    chunk = binReader.ReadBytes(bufferSize);
+                    if (data == null) data = chunk;
+                    index = binReader.BaseStream.Position;
+                } catch (IOException ex) {
+                    Thread.Sleep(DELAY);
 
-                byte[] chunk = binReader.ReadBytes(1024);
-                data = chunk;
+                    if (ex.HResult == -2147023890) {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Случилось страшное!");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        goto TryAgain1;
+                    } else {
+                        Debug.WriteLine("TA 2: " + ex.HResult);
+                        goto TryAgain2;
+                    }
+                }
+
+
                 while (chunk.Length > 0) {
-                    chunk = binReader.ReadBytes(1024);
-                    data = CombineByteArrays(data, chunk);
+                    TryAgain3: try {
+                        chunk = binReader.ReadBytes(bufferSize);
+                        index = binReader.BaseStream.Position;
+                        data = CombineByteArrays(data, chunk);
+                        Console.Write("\r{0} / {1}", data.Length, length);
+                    } catch (IOException ex) {
+                        Thread.Sleep(DELAY);
+                        if (!di.IsReady) { fin.Close(); goto TryAgain1; }
+                        Debug.WriteLine("TA 3: " + ex.Message + ex.HResult);
+                        goto TryAgain3;
+                    }
                 }
             }
 
 
             File.WriteAllBytes(name, data);
 
-
-            //} catch (Exception ex) {
-            //    Debug.WriteLine(ex.Message);
-            //    Thread.Sleep(200);
-            //    goto TryAgain1;
-            //}
-            currentByte = 0;
             fin.Close();
+            Console.Write(" ... OK");
+            Console.WriteLine();
         }
-
 
 
         public static byte[] CombineByteArrays(byte[] first, byte[] second)
